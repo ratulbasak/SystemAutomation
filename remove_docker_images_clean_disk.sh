e018ac031c4d45a4b97a3a0cc76582f616762c8d
ratulbasak93 <ratulbasak93@gmail.com>
2019-01-24 23:23:40 +0600

    added shell script

---
+++ remove_docker_images_clean_disk.sh
@@ -0,0 +1,32 @@
+#===============================================================================
+#
+#          FILE: remove_docker_images_clean_disk.sh
+# 
+#         USAGE: ./remove_docker_images_clean_disk.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: remove all docker images when disk space above 80%
+#        AUTHOR: Ratul Basak, 
+#  ORGANIZATION: 
+#       CREATED: বৃহস্পতিবার 24 জানু 2019 11:19:52  +06
+#      REVISION:  ---
+#===============================================================================
+
+#!/bin/bash
+
+volume='/dev/xvda1'
+space=$(df -H $volume | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $5 }')
+cmd=$(df -H $volume | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $5 }' | cut -d'%' -f1)
+
+if [ $cmd -ge 80 ]; then
+   echo "Running out of space. Deleting docker images"
+   docker rm $(docker ps -a -q)
+   docker rmi $(docker images -aq)
+else
+   echo "Diskspace is available $volume : $space"
+fi
+
