5b634798f3f4b5ffa78e6b53531fe07651fae800
ratulbasak93 <ratulbasak93@gmail.com>
2019-01-24 23:31:37 +0600

    added shell script

---
+++ ecr_login.sh
@@ -0,0 +1,28 @@
+#===============================================================================
+#
+#          FILE: ecr_login.sh
+# 
+#         USAGE: ./ecr_login.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: awscli [aws configure]
+#          BUGS: ---
+#         NOTES: Login to aws ecr
+#        AUTHOR: Ratul Basak, 
+#  ORGANIZATION: 
+#       CREATED: বৃহস্পতিবার 24 জানু 2019 11:28:06  +06
+#      REVISION:  ---
+#===============================================================================
+
+#!/bin/bash
+
+export docker_server=https://AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com
+export token=$(aws ecr get-authorization-token --query authorizationData[].authorizationToken --output text | base64 -d | cut -d: -f2)
+
+#export login=`echo $aws_login`
+
+#$login
+docker login -u AWS -p $token $docker_server
+
